21309e67f5a83eb0ac64e20e4a03d73373d3c24b
xinlei.chen <xinlei.chen@bianlifeng.com>
2017-11-09 13:53:55 +0800

    删除缓存

--- www/.cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/shinley/Downloads/gatsby-master/www/src/layouts/index.js"
-  import data from "/Users/shinley/Downloads/gatsby-master/www/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
