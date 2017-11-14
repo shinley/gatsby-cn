bccd705c8599b982ecb33fd18c4616f460e62d0d
xinlei.chen <xinlei.chen@bianlifeng.com>
2017-11-14 13:23:37 +0800

    删除文件

--- packages/gatsby-plugin-google-analytics/src/gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-exports.onRouteUpdate = function({ location }) {
-  // Don't track while developing.
-  if (process.env.NODE_ENV === `production` && typeof ga === `function`) {
-    window.ga(`set`, `page`, (location || {}).pathname)
-    window.ga(`send`, `pageview`)
-  }
-}
