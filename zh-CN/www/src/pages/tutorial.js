3deb16acc8c9bc7ce37c7f2478a1253c3c72bb8c
xinlei.chen <xinlei.chen@bianlifeng.com>
2017-11-19 16:58:03 +0800

    翻译教程首页

--- zh-CN/www/src/pages/tutorial.js
+++ zh-CN/www/src/pages/tutorial.js
@@ -4,47 +4,40 @@ import Container from "../components/container"
 
 export default () => (
   <Container>
-    <h1 css={{ marginTop: 0 }}>Gatsby.js Tutorial</h1>
+    <h1 css={{ marginTop: 0 }}>Gatsby.js 教程</h1>
     <p>
-      Hi! We’re so happy you decided to try using Gatsby. This tutorial has (or
-      rather will have once all parts are written) five parts that'll walk you
-      from getting started developing and building Gatsby sites to deploying a
-      finished and polished high performance static PWA.
+      嗨！ 我们很高兴你决定尝试使用盖茨比。 本教程已经有（所有部分都将被编写）五个部分，
+      从开始开发和构建Gatsby站点到部署成品和优雅的高性能静态PWA，
     </p>
     <p>
-      This tutorial is for <em>everyone</em>! You do not need to be a programmer
-      or React.js expert. We'll walk you through things.
+    本教程适用于<em>所有人</em>！ 你不需要成为程序员或React.js专家。 我们会带你走过去的
     </p>
 
     <ol>
       <li>
-        <Link to="/tutorial/part-one/">Introduction to basics of Gatsby</Link>
-        {` `}— Starting new projects, developing, and deploying sites.
+        <Link to="/tutorial/part-one/">Gatsby的基本介绍</Link>
+        {` `}— 开始新项目，开发和部署网站。
       </li>
       <li>
         <Link to="/tutorial/part-two/">
-          Introduction to using CSS in Gatsby
-        </Link>. Explore libraries like Typography.js, CSS Modules, Glamor, and
-        Styled Components.
+        在Gatsby中使用CSS介绍
+        </Link>.  开发像Typography.js，CSS模块，Glamor，和样式化的组件的库。
       </li>
       <li>
         <Link to="/tutorial/part-three/">
-          Explore building nested layouts in Gatsby
-        </Link>. Layouts are sections of your site that are reused across
-        multiple pages like headers and footers.
+          在Gatsby中开发构建内嵌的布局
+        </Link>. 布局是网站的一部分，可以在多个页面（如页眉和页脚）之间重复使用。
       </li>
       <li>
         <Link to="/tutorial/part-four/">
-          Learn how to work with Gatsby's data layer.
+          了解如何使用Gatsby的数据层。
         </Link>
         {` `}
-        Explore source & transformer plugins. Get introduced to programmatic
-        pages and how to write GraphQL queries. In this part of the tutorial
-        we'll build a simple markdown blog.
+        开发源代码和transformer插件。 介绍编程页面以及如何编写GraphQL查询。 
+        在本教程的这一部分，我们将构建一个简单的MarkDown博客。
       </li>
       <li>
-        Finishing and deploying a website. React Helmet. We walk through how to
-        put the finishing touches on a website project.
+      完成并部署一个网站。 React Helmet。 我们将介绍如何在网站项目上进行最后的修改。
       </li>
     </ol>
   </Container>
