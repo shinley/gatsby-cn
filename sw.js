e34f57278cb0ee1a6a80d171667ddc8025083f35
xinlei.chen <xinlei.chen@bianlifeng.com>
2017-11-28 18:51:09 +0800

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-38d0a8f7581421f25753.js","04e690b45ec7893f1a16b42abbee8b4e"],["/app-48e37bbdf584afe0d102.js","f69f82356e7ef81e71d3ae501e98701c"],["/app-869e3f877b79cc658216.js","16946855f137b6703e413b239deaba6e"],["/commons-2ffb1ba303cafc6db07b.js","ed5ddda56800fe41447838caf4e789ba"],["/index.html","bb182ebb405767fbc4ad78db27e815dd"],["/manifest.json","706307414b118ea10206ec9f1f91aad1"],["/offline-plugin-app-shell-fallback/index.html","c755068f7d6451145f2fbf5fecf1e6ed"]];
+var precacheConfig = [["/app-38d0a8f7581421f25753.js","04e690b45ec7893f1a16b42abbee8b4e"],["/app-48e37bbdf584afe0d102.js","f69f82356e7ef81e71d3ae501e98701c"],["/app-869e3f877b79cc658216.js","16946855f137b6703e413b239deaba6e"],["/commons-2ffb1ba303cafc6db07b.js","ed5ddda56800fe41447838caf4e789ba"],["/index.html","2b3e0fa18036d076a89897ed6057afce"],["/manifest.json","706307414b118ea10206ec9f1f91aad1"],["/offline-plugin-app-shell-fallback/index.html","8578fda040003fdc6b4999cae8df810c"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
