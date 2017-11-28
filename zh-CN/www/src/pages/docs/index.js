94582a60840c4f39bd34cb5882ae437d20efea88
xinlei.chen <xinlei.chen@bianlifeng.com>
2017-11-28 18:49:30 +0800

    修改doc index

--- zh-CN/www/src/pages/docs/index.js
+++ zh-CN/www/src/pages/docs/index.js
@@ -27,39 +27,34 @@ class IndexRoute extends React.Component {
             <code>cd gatsby-site</code>
           </li>
           <li>
-            <code>gatsby develop</code> — Gatsby will start a hot-reloading
-            development environment accessible at <code>localhost:8000</code>
+            <code>gatsby develop</code> —Gatsby会启动一个热加载的开发环境，可以通过地址 <code>localhost:8000</code>访部
           </li>
           <li>
-            Try editing the javascript pages in <code>src/pages</code>. Saved
-            changes will live reload in the browser.
+            尝试编辑<code>src/pages</code>目录下的javascript页面。保存修改后浏览器会实时加载修改。
           </li>
           <li>
-            <code>gatsby build</code> — Gatsby will perform an optimized
-            production build for your site generating static HTML and per-route
-            JavaScript code bundles.
+            <code>gatsby build</code> — Gatsby将执行优化
+            为您的网站生成静态HTML和按路由生成JavaScript代码绑定。
           </li>
           <li>
-            <code>gatsby serve</code> — Gatsby starts a local HTML server for
-            testing your built site.
+            <code>gatsby serve</code> — Gatsby启动一个本地的HTML服务器测试你的网站。
           </li>
         </ol>
-        <h2>Using other starters</h2>
+        <h2>使用其它 starters</h2>
         <p>
-          Running <code>gatsby new</code> installs the default Gatsby starter.
-          There are{` `}
+          运行 <code>gatsby new</code> 安装默认的 Gatsby starter.
+          有{` `}
           <Link to="/docs/gatsby-starters/">
-            many other official and community starters
+            很多其它的官方的或者社区提供的starters
           </Link>
           {` `}
-          you can use to kickstart building your Gatsby site.
+          你可以使用来构建你的Gatsby站点。
         </p>
         <h2>Work through the tutorial</h2>
         <p>
-          It walks you through building a Gatsby site from scratch to a finished
-          polished site.
+          它带着你从头开始建造一个Gatsby漂亮的网站。
           {` `}
-          <Link to="/tutorial/">Go to the tutorial</Link>.
+          <Link to="/tutorial/">查看教程</Link>.
         </p>
         <div
           css={{
@@ -69,7 +64,7 @@ class IndexRoute extends React.Component {
             },
           }}
         >
-          <h2>Documentation</h2>
+          <h2>文档</h2>
           <SidebarBody inline yaml={docsSidebar} />
         </div>
       </Container>
